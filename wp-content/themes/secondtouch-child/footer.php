2d0eac758e3b3680b3ab05fa9cfa310e9d7b34db
Vahram Martinyan <vahram.martinyan@gmail.com>
2017-05-31 20:01:07 +0400

    31052017-20:00

--- wp-content/themes/secondtouch-child/footer.php
+++ wp-content/themes/secondtouch-child/footer.php
@@ -38,16 +38,16 @@
             echo do_shortcode( $options["copyright_footer"] ); ?>
         </div>
 
-        <div class="six columns">
+        <!--<div class="six columns">
 			<div style="float:right">
 				<span style="display:inline-block; padding-top:5px;"><?php echo __('[:hy]մշակված է[:en]designed by[:ru]разработан[:]')?></span>
 				<a href="http://hf.am/" target="_blank" style="float:right; margin-left:10px;">
 					<img width="29" src="<?php echo site_url(); ?>/wp-content/uploads/2013/02/hf-logo.png"/>
 				</a>
 			</div>
-            <!--<?php wp_nav_menu(array('theme_location' => 'footer_menu','depth'=> 1, 'container' => 'nav', 'fallback_cb' => 'false', 'menu_class' => 'footer-menu', 'walker' => new crum_clean_walker())); ?>-->
+            <--!--<?php wp_nav_menu(array('theme_location' => 'footer_menu','depth'=> 1, 'container' => 'nav', 'fallback_cb' => 'false', 'menu_class' => 'footer-menu', 'walker' => new crum_clean_walker())); ?>--!fgh>
 
-        </div>
+        </div>-->
     </div>
 </section>
 <a href="#" id="linkTop" class="backtotop hidden"></a>
